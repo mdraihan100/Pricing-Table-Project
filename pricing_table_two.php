41410805268c635ad37090b93824d55cd557fc87
Raihan Hossen <devraihan25@gmail.com>
2024-07-09 19:18:54 +0600

    template intitiate

--- pricing_table_two.php
+++ pricing_table_two.php
@@ -60,50 +60,50 @@ add_action('add_meta_boxes', 'pricing_table_meta_box');
 
 function pricing_table_details_callback($post) {
     // Add nonce for security and authentication.
-    wp_nonce_field('pricing_table_nonce_action', 'pricing_table_nonce'); // this line//
+    wp_nonce_field('pricing_table_nonce_action', 'pricing_table_nonce'); 
 
     // Retrieve existing values from the database.
-    $subtitle = get_post_meta($post->ID, 'pricing_table_title', true);
-    $short_description = get_post_meta($post->ID, 'pricing_table_description', true);
-    $price = get_post_meta($post->ID, 'pricing_table_price', true);
-    $frequency = get_post_meta($post->ID, 'pricing_table_frequency_item', true);
-    $button_text = get_post_meta($post->ID, 'pricing_table_button', true);
-    $button_url = get_post_meta($post->ID, 'pricing_button_url', true);
-    $css_classes = get_post_meta($post->ID, 'pricing_button_classes', true);
-    $upload_icon = get_post_meta($post->ID, 'pricing_button_icon', true);
+    $subtitle = get_post_meta($post->ID, 'pricing_table_title_1', true);
+    $short_description = get_post_meta($post->ID, 'pricing_table_description_1', true);
+    $price = get_post_meta($post->ID, 'pricing_table_price_1', true);
+    $frequency = get_post_meta($post->ID, 'pricing_table_frequency_item_1', true);
+    $button_text = get_post_meta($post->ID, 'pricing_table_button_1', true);
+    $button_url = get_post_meta($post->ID, 'pricing_button_url_1', true);
+    $css_classes = get_post_meta($post->ID, 'pricing_button_classes_1', true);
+    // $upload_icon = get_post_meta($post->ID, 'pricing_button_icon', true);
     
     // Display fields
     ?>
     <table>
         <tr>
             <td>Add Title:</td>
-            <td><input type="text" name="pricing_table_title" value="<?php echo esc_attr($subtitle); ?>"></td>
+            <td><input type="text" name="pricing_table_title_1" value="<?php echo esc_attr($subtitle); ?>"></td>
         </tr>
         <tr>
             <td> Description:</td>
-            <td><input type="text" name="pricing_table_description" value="<?php echo esc_attr($short_description); ?>"></td>
+            <td><input type="text" name="pricing_table_description_1" value="<?php echo esc_attr($short_description); ?>"></td>
         </tr>
         <tr>
             <td>Price:</td>
-            <td><input type="number" name="pricing_table_price" value="<?php echo esc_attr($price); ?>"></td>
+            <td><input type="number" name="pricing_table_price_1" value="<?php echo esc_attr($price); ?>"></td>
         </tr>
         <tr>
             <td>Pricing Frequency Item:</td>
-            <td><input type="text" name="pricing_table_frequency_item" value="<?php echo esc_attr($frequency); ?>"></td>
+            <td><input type="text" name="pricing_table_frequency_item_1" value="<?php echo esc_attr($frequency); ?>"></td>
         </tr>
      
         <tr>
             <td>Button Text:</td>
-            <td><input type="text" name="pricing_table_button" value="<?php echo esc_attr($button_text); ?>"></td>
+            <td><input type="text" name="pricing_table_button_1" value="<?php echo esc_attr($button_text); ?>"></td>
         </tr>
         <tr>
             <td>Button URL:</td>
-            <td><input type="text" name="pricing_button_url" value="<?php echo esc_attr($button_url); ?>"></td>
+            <td><input type="text" name="pricing_button_url_1" value="<?php echo esc_attr($button_url); ?>"></td>
         </tr>
    
         <tr>
             <td>CSS Classes:</td>
-            <td><input type="text" name="pricing_button_classes" value="<?php echo esc_attr($css_classes); ?>"></td>
+            <td><input type="text" name="pricing_button_classes_1" value="<?php echo esc_attr($css_classes); ?>"></td>
         </tr>
         <h1><?php echo "Hello Pricing Table New" ?></h1>
     </table>
@@ -137,24 +137,50 @@ function bpt_save_pricing_table_meta($post_id) {
     }
 
     // Sanitize user input.
-    $subtitle = sanitize_text_field($_POST['pricing_table_title']);
-    $short_description = sanitize_text_field($_POST['pricing_table_description']);
-    $price = sanitize_text_field($_POST['pricing_table_price']);
-    $frequency = sanitize_text_field($_POST['pricing_table_frequency_item']);
-    $button_text = sanitize_text_field($_POST['pricing_table_button']);
-    $button_url = sanitize_text_field($_POST['pricing_button_url']);
-    $css_classes = sanitize_text_field($_POST['pricing_button_classes']);
+    $subtitle = sanitize_text_field($_POST['pricing_table_title_1']);
+    $short_description = sanitize_text_field($_POST['pricing_table_description_1']);
+    $price = sanitize_text_field($_POST['pricing_table_price_1']);
+    $frequency = sanitize_text_field($_POST['pricing_table_frequency_item_1']);
+    $button_text = sanitize_text_field($_POST['pricing_table_button_1']);
+    $button_url = sanitize_text_field($_POST['pricing_button_url_1']);
+    $css_classes = sanitize_text_field($_POST['pricing_button_classes_1']);
 
     // Update the meta fields.
-    update_post_meta($post_id, 'pricing_table_title', $subtitle);
-    update_post_meta($post_id, 'pricing_table_description', $short_description);
-    update_post_meta($post_id, 'pricing_table_price', $price);
-    update_post_meta($post_id, 'pricing_table_frequency_item', $frequency);
-    update_post_meta($post_id, 'pricing_table_button', $button_text);
-    update_post_meta($post_id, 'pricing_button_url', $button_url);
-    update_post_meta($post_id, 'pricing_button_classes', $css_classes);
-
-    // some change doing to check the github 
+    update_post_meta($post_id, 'pricing_table_title_1', $subtitle);
+    update_post_meta($post_id, 'pricing_table_description_1', $short_description);
+    update_post_meta($post_id, 'pricing_table_price_1', $price);
+    update_post_meta($post_id, 'pricing_table_frequency_item_1', $frequency);
+    update_post_meta($post_id, 'pricing_table_button_1', $button_text);
+    update_post_meta($post_id, 'pricing_button_url_1', $button_url);
+    update_post_meta($post_id, 'pricing_button_classes_1', $css_classes);  
 }
 
-?>
+add_shortcode( 'pricing_table','shortcode_function'  );
+function shortcode_function( $atts ) {
+    $a = shortcode_atts( array(
+		'post_id' => null,  //default value
+        'template' => 'template_one' //default value
+	), $atts );
+
+    $template_name = $atts['template'];
+
+    ob_start();
+
+    // var_dump($template_name);
+    // var_dump(get_post_meta($atts["post_id"], "pricing_table_title_1",  true));
+include("templates/$template_name.php");
+
+
+return ob_get_clean();
+}
+
+// Enque style.cssfile // 
+add_action('wp_enqueue_scripts', 'enque_stylesheet_file');
+
+function enque_stylesheet_file() {
+    wp_enqueue_style( 'prefix-style', plugins_url('style.css', __FILE__));   
+
+    $plugin_url = plugin_dir_url( __FILE__ );
+    wp_enqueue_style( 'style',  $plugin_url . "responsive.css");
+}
+
---
+++ templates/template_two.php
@@ -0,0 +1,62 @@
+<!-- 
+
+<div class="container main_section">
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+            <div class="pricing_table_heading">
+                <h2>Please choose a plan below</h2>
+
+                <div class="pricing_header_button">
+                    <button class="pricing_heading-button">Monthly</button>
+                    <div class="button_right_text">
+                        <h5>Yearly</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2"></div>
+    </div>
+</div> -->
+
+
+    <div class="container">
+    <div class="pricing-table">
+        <div class="row">
+            <div class="col-md-4 col-sm-12 pricing pricing_column">
+                <div class="pricing_div">
+                    <h1>Standard</h1>
+                </div>
+
+                <div class="pricing-card basic">
+                    <div class="card-header">
+                        <div class="pricing_content_basic">
+                            <div class="price">
+                                <h1>$09.99</h1>
+                                <p>per month</p>
+                            </div>
+                            <ul class="features">
+                                <li> <i class="fas fa-check check_icon"></i> Sample Text Here</li>
+                                <li> <i class="fas fa-check check_icon"></i> Other Text Title</li>
+                                <li> <i class="fas fa-close check_cross_icon"></i> Text Space Goes Here</li>
+                                <li> <i class="fas fa-close check_cross_icon"></i> Description Space</li>
+                                <li> <i class="fas fa-close check_cross_icon"></i> Sample Text Here</li>
+
+                            </ul>
+                            <div class="pricing_button">
+                                <button class="btn">Select</button>
+                                <img src="./img/Layer_1 (3).png" alt="">
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+          
+
+        </div>
+    </div>
+</div>
+        
+
